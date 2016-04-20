3188cddcd7b9a200131a860a587ca713cccc7693
RC <RelentlessCoffee@gmail.com>
2016-04-20 00:03:49 -0700

    imported json and added product_data

--- script.py
+++ script.py
@@ -4,15 +4,17 @@
 # response = requests.get("url")
 import requests
 import bs4
+import json
 
 
 def scrape(url):
     response = requests.get(url)
     body = response.text
     soup = bs4.BeautifulSoup(body, 'html.parser')
-    return soup
+    form = soup.find("form", class_="variations_form cart")
+    product_data = form['data-product_variations']
+    product_data = json.loads(product_data)
+    return product_data[1]["max_qty"]
 
-url = "http://www.white2tea.com/tea-shop/2016-fade-raw-puer-huangpian-brick/"
-page = scrape(url)
-
-print(page.title.string)
+url = "http://www.white2tea.com/tea-shop/2015-little-walk/"
+print(scrape(url))
