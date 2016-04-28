14f0b978406f58371b6c3780f6d7637c4c6a3286
RC <RelentlessCoffee@gmail.com>
2016-04-27 18:22:00 -0700

    filter out non-product urls

--- catalog.py
+++ catalog.py
@@ -12,7 +12,11 @@ def find_product_urls(soup):
     product_element = soup.find('ul', class_="products")
     links = product_element.find_all('a')
     links = set([link["href"] for link in links])
-    return links
+    product_links = []
+    for link in links:
+        if link.startswith("http"):
+            product_links.append(link)
+    return product_links
 
 url = "http://www.white2tea.com/tea-shop/product-category/raw-puer-tea/white2tea-raw-puer-tea/2015-white-2-tea-puer-teas/"
 page = load(url)
