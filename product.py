50c4fd4075e27d3d8ff25c0a1b45246fbb361c1c
RC <RelentlessCoffee@gmail.com>
2016-04-27 18:51:05 -0700

    removed commented out code

--- product.py
+++ product.py
@@ -13,10 +13,6 @@ def find_quantities(soup):
     form = soup.find("form", class_="variations_form cart")
     product_data = form['data-product_variations']
     product_data = json.loads(product_data)
-    # quantities = []
-    # for option in product_data:
-    #    quantity = option["max_qty"]
-    #    quantities.append(quantity)
     quantities = [option["max_qty"] for option in product_data]
     options = soup.find_all("option")[1:]
     options = [option["value"] for option in options]
