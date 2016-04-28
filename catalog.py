f62a27693ff75c3d72e3d2291c64f45a239ea0ae
RC <RelentlessCoffee@gmail.com>
2016-04-27 17:52:53 -0700

    found the first page links

--- catalog.py
+++ catalog.py
@@ -9,3 +9,11 @@ def load(url):
 
 
 def find_product_urls(soup):
+    product_element = soup.find('ul', class_="products")
+    links = product_element.find_all('a')
+    links = set([link["href"] for link in links])
+    return links
+
+url = "http://www.white2tea.com/tea-shop/product-category/raw-puer-tea/white2tea-raw-puer-tea/2015-white-2-tea-puer-teas/"
+page = load(url)
+print(find_product_urls(page))
