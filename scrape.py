cc0836ceb958f5752976955af7fac8b5a3eca723
RC <RelentlessCoffee@gmail.com>
2016-05-11 23:20:41 -0700

    scrape entire white2tea raw puer category

--- scrape.py
+++ scrape.py
@@ -1,10 +1,17 @@
 from catalog import find_year_urls, find_product_urls
-from product import load_single_page, find_quantities, find_name
+from product import load_single_page, find_quantities, find_name, NoMaxQuantity
 
 
 category_url = "http://www.white2tea.com/tea-shop/product-category/white2tea-raw-puer-tea/"
 year_urls = find_year_urls(category_url)
+
 for year_url in year_urls:
     product_urls = find_product_urls(year_url)
+
     for product_url in product_urls:
-        print(product_url)
+        try:
+            page = load_single_page(product_url)
+            print(find_name(page))
+            print(find_quantities(page))
+        except NoMaxQuantity as exception:
+            print(exception)
