e42e98e164912099920e47ca9c445cde157bd589
RC <RelentlessCoffee@gmail.com>
2016-05-11 23:55:00 -0700

    Improve product.py error handling when finding quantities

--- product.py
+++ product.py
@@ -15,6 +15,8 @@ def load_single_page(url):
 
 def _find_quantities_from_form(soup):
     form = soup.find("form", class_="variations_form cart")
+    if form is None:
+        raise NoMaxQuantity("no max quantity")
     product_data = form['data-product_variations']
     product_data = json.loads(product_data)
     quantities = [option["max_qty"] for option in product_data]
@@ -25,6 +27,8 @@ def _find_quantities_from_form(soup):
 
 def _find_quantities_from_input(soup):
     product_data = soup.find("input", class_="input-text qty text")
+    if product_data is None:
+        raise NoMaxQuantity("no max quantity")
     quantity = product_data['max']
     if quantity == "":
         raise NoMaxQuantity("no max quantity")
@@ -34,7 +38,7 @@ def _find_quantities_from_input(soup):
 def find_quantities(soup):
     try:
         return _find_quantities_from_form(soup)
-    except TypeError:
+    except NoMaxQuantity:
         return _find_quantities_from_input(soup)
 
 
