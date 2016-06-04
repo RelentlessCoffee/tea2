4adb917bbc0cf2b787fa6ee4ccdb390b9d2b5067
RC <RelentlessCoffee@gmail.com>
2016-06-04 02:26:36 -0700

    scrape.py saves to disk #3

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 from catalog import find_year_urls, find_product_urls
-from product import load_single_page, find_quantities, find_name, NoMaxQuantity
-
+from product import load_single_page, find_options, find_name, NoMaxQuantity
+from storage import save
 
 category_urls = [
     "http://www.white2tea.com/tea-shop/product-category/raw-puer-tea/misc-raw-puer-tea/",
@@ -21,17 +21,27 @@ year_urls = [
 ]
 product_urls = []
 
+output_filename = "output"
+data = {"products": []}
+
 for category_url in category_urls:
     year_urls.extend(find_year_urls(category_url))
 
 for year_url in year_urls:
     product_urls.extend(find_product_urls(year_url))
 
+print("done finding product urls")
+
 for product_url in product_urls:
     try:
         page = load_single_page(product_url)
         name = find_name(page)
-        quantities = find_quantities(page)
-        print("{} {}".format(name, quantities))
+        options = find_options(page)
+        single_product = {
+            "name": name,
+            "options": options
+        }
+        data["products"].append(single_product)
     except NoMaxQuantity as exception:
         print(str(exception).upper())
+save(data, output_filename)
