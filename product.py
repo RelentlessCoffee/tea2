5e8b6132671715acde54f3957c8c41b0ddf68061
RC <RelentlessCoffee@gmail.com>
2016-06-04 02:25:02 -0700

    restructure find_output, rename find_quantities

--- product.py
+++ product.py
@@ -13,7 +13,7 @@ def load_single_page(url):
     return soup
 
 
-def _find_quantities_from_form(soup):
+def _find_options_from_form(soup):
     form = soup.find("form", class_="variations_form cart")
     if form is None:
         raise NoMaxQuantity("no max quantity")
@@ -22,24 +22,34 @@ def _find_quantities_from_form(soup):
     quantities = [option["max_qty"] for option in product_data]
     options = soup.find_all("option")[1:]
     options = [option["value"] for option in options]
-    return list(zip(options, quantities))
+    # return list(zip(options, quantities))
+    option_dicts = []
+    for option, quantity in zip(options, quantities):
+        option_dicts.append({
+            "name": option,
+            "quantity": quantity
+            })
+    return option_dicts
 
 
-def _find_quantities_from_input(soup):
+def _find_options_from_input(soup):
     product_data = soup.find("input", class_="input-text qty text")
     if product_data is None:
         raise NoMaxQuantity("no max quantity")
     quantity = product_data['max']
     if quantity == "":
         raise NoMaxQuantity("no max quantity")
-    return quantity
+    return [{
+        "name": find_name(soup),
+        "quantity": int(quantity)
+        }]
 
 
-def find_quantities(soup):
+def find_options(soup):
     try:
-        return _find_quantities_from_form(soup)
+        return _find_options_from_form(soup)
     except NoMaxQuantity:
-        return _find_quantities_from_input(soup)
+        return _find_options_from_input(soup)
 
 
 def find_name(soup):
